b0693ba12382bf0b3930ebe8fe2cf5246ef80140
SantanaLari <88253169+SantanaLari@users.noreply.github.com>
2023-01-28 23:05:07 -0300

    Create 5- manipulandoTuplas_frutas.py

---
+++ 5- manipulandoTuplas_frutas.py
@@ -0,0 +1,8 @@
+#Crie uma tupla com 5 nomes de frutas e, 
+#usando um laço "for", imprima cada fruta em uma linha diferente.
+
+frutas = ('Pera', 'Uva', 'Maçã', 'Manga', 'Banana')
+
+for nome in frutas:
+  print(f'Fui ao mercado e comprei: {nome}')
+  
